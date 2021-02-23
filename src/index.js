43edad29a0f782715819629fd890e1a31b5506ae
picassonk21 <picassonk21@gmail.com>
2021-02-24 01:35:23 +0300

    init: start typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length < 1) return 0
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length < 1) return 0
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length < 1) return 0
+  return array.reduce(((sum, value) => sum + value), 0) / array.length;
 }
